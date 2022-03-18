55e26b62fe936f12ac9c6228b84e8ce5fa9c9396
TonyQuesada <Tonyquesadaxd@gmail.com>
2022-03-18 17:40:01 -0600

    Update enviar_requisitos.php

--- PHP/enviar_requisitos.php
+++ PHP/enviar_requisitos.php
@@ -5,30 +5,34 @@ include '../conection.php';
 session_start();
 setlocale(LC_TIME, "spanish");
 
-require_once ('../PHPMailer/PHPMailerAutoload.php');
+$result = "";
+require '../PHPMailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer();
 //indico a la clase que use SMTP
 $mail -> IsSMTP();
 //permite modo debug para ver mensajes de las cosas que van ocurriendo
-$mail -> SMTPDebug = 2;
+// $mail -> SMTPDebug = 2;
 //Debo de hacer autenticación SMTP
 $mail -> SMTPAuth = true;
-$mail -> SMTPSecure = "ssl";
+$mail -> SMTPSecure = "tls";
 //indico el servidor de Gmail para SMTP
 $mail -> Host = "smtp.gmail.com";
 //indico el puerto que usa Gmail
-$mail -> Port = 465;
+$mail -> Port = 587;
+//
+$mail -> isHTML(true);
 //indico un usuario / clave de un usuario de gmail
 $mail -> Username = "TonyQuesadaxd@gmail.com";
 $mail -> Password = "Rock123456789";
 $mail -> SetFrom('TonyQuesadaxd@gmail.com', 'Administración de BAJA');
 $mail -> AddReplyTo("TonyQuesadaxd@gmail.com","Administración de BAJA");
 $mail -> Subject = "Envío de email usando SMTP de Gmail";
-$mail -> MsgHTML("Hola que tal, esto es el cuerpo del mensaje!");
+$mail -> body = "Hola que tal, esto es el cuerpo del mensaje!";
 //indico destinatario
 $address = ($_SESSION['u_Correo_electronico']);
 $mail-> AddAddress($address, ($_SESSION['u_Nombre_Usuario']));
+
 if(!$mail-> Send()) {
 echo "Error al enviar: " . $mail -> ErrorInfo;
 } else {
