3e610a0e331e59b3d4ba22553f79e027c4426a53
TonyQuesada <Tonyquesadaxd@gmail.com>
2022-03-18 18:35:41 -0600

    Update enviar_requisitos.php

--- PHP/enviar_requisitos.php
+++ PHP/enviar_requisitos.php
@@ -8,8 +8,8 @@ setlocale(LC_TIME, "spanish");
 require '../PHPMailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer();
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = 'intelligent-hellman.138-59-135-33.plesk.page';
+$mail->IsSMTP();
 $mail->Port = 587;
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'tls';
