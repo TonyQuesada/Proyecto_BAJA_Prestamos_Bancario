347b806ad306b6c9b6298b654d3b3802ba8acd72
TonyQuesada <Tonyquesadaxd@gmail.com>
2022-03-18 18:51:19 -0600

    Update enviar_requisitos.php

--- PHP/enviar_requisitos.php
+++ PHP/enviar_requisitos.php
@@ -5,38 +5,39 @@ include '../conection.php';
 session_start();
 setlocale(LC_TIME, "spanish");
 
-require '../PHPMailer/PHPMailerAutoload.php';
+date_default_timezone_set('Amrerica/Costa Rica');
+require_once '../PHPMailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer();
-$mail->Host = 'intelligent-hellman.138-59-135-33.plesk.page';
 $mail->isSMTP();
-$mail->Port = 587;
-$mail->SMTPAuth = true;
+$mail->SMTPDebug = 2;
+$mail->CharSet  = 'UTF-8';
+
+$mail->Host = 'localhost';
+$mail->Port = 25;
 $mail->SMTPSecure = 'tls';
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer'  => true,
+        'verify_depth' => 3,
+        'allow_self_signed' => true,
+        'peer_name' => 'Plesk',
+    )
+);
+
+$mail->SMTPAuth = true;
 $mail->Username = "TonyQuesadaxd@gmail.com";
 $mail->Password = "Rock123456789";
 
 $mail->setFrom('TonyQuesadaxd@gmail.com', 'Administración de BAJA');
 $mail->addAddress(($_SESSION['u_Correo_electronico']), ($_SESSION['u_Nombre_Usuario']));
-$mail->addReplyTo("TonyQuesadaxd@gmail.com", "Administración de BAJA");
-
-$mail->isHTML(true);
 $mail->Subject = 'Envío de email usando SMTP de Gmail';
 $mail->msgHTML('Email content with <strong>html</strong>');
+
+// $mail->addReplyTo("TonyQuesadaxd@gmail.com", "Administración de BAJA");
+// $mail->isHTML(true);
 // $mail->Body = 'Hola que tal, esto es el cuerpo del mensaje!';
 // $mail->MsgHTML("Hola que tal, esto es el cuerpo del mensaje!");
-
-
-$mail->SMTPOptions = array(
-    'ssl' => array(
-    'verify_peer' => false,
-    'verify_peer_name' => false,
-    'allow_self_signed' => true,
-    'peer_name' => 'Plesk'
-   )
- );
- $mail->SMTPDebug = 2;
- $mail->CharSet  = 'UTF-8';
 
 
 if (!$mail->send()) {
